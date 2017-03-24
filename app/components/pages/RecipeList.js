fc07848812c1b66882434ffdcc02369ea2f81823
J. Motyl <webdev@stupiddog.net>
2017-03-24 02:04:29 -0500

    WIP, recipe list works with mock data, added create button

--- app/components/pages/RecipeList.js
+++ app/components/pages/RecipeList.js
@@ -3,24 +3,37 @@
     
 */
 import React from 'react';
-import { Grid, Row, Col, Panel } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, ButtonToolbar, Button } from 'react-bootstrap';
 
 import Recipes from './panels/Recipes.js';
 
+const title = (<span><h2>Recipes</h2> <i>Click to edit</i></span>);
+
+
 //  md={ 8 } mdOffset={ 1 }
 export default class RecipeList extends React.Component {
-  render() {
-    return (
-        <Grid>
-            <Row className="show-grid">
-                <Col>
-                    <Panel header={ 'Recipes' }>
-                        <Recipes/>
-                    </Panel>
-                </Col>
-            </Row>
-        </Grid>
-    );
-  }
+
+    createClick() {
+        console.log('create recipe click');
+    }
+
+    render() {
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Col>
+                        <Panel header={title}  bsStyle="success">
+                            <Recipes/>
+                        </Panel>
+                    </Col>
+                </Row>
+                <Row><Col>
+                    <ButtonToolbar>
+                        <Button bsStyle="success" onClick={this.createClick}>Create a Recipe</Button>
+                    </ButtonToolbar>
+                </Col></Row>
+            </Grid>
+        );
+    }
 }
 
